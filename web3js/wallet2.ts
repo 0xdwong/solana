a1d05962dc920d459f9cccf89cb4d93da794fa64
0xdwong <0xdwong@gmail.com>
2024-03-06 17:55:25 +0800

    add: gen atas

--- web3js/wallet2.ts
+++ web3js/wallet2.ts
@@ -1,19 +1,52 @@
-import { Keypair, LAMPORTS_PER_SOL, Connection } from "@solana/web3.js";
+import { Keypair, LAMPORTS_PER_SOL, Connection, PublicKey } from "@solana/web3.js";
+import { getOrCreateAssociatedTokenAccount, createTransferInstruction } from "@solana/spl-token";
 import * as fs from 'fs';
 import dotenv from "dotenv"
 dotenv.config();
 
-function genAddress(){
+
+import secret from './guideSecret.json';
+const signer = Keypair.fromSecretKey(new Uint8Array(secret));
+const SOLANA_CONNECTION = new Connection(process.env.RPC_ENDPOINT || '', {
+    commitment: "confirmed",
+});
+const MINT_ADDRESS = 'Lg2rBEfVDS8K56gc3bWrUibQoGwTiCQ9mpmSJ75v75m'; //You must change this value!
+console.log('address', signer.publicKey.toString());
+
+async function genAddress() {
+    //accunt
     const keypair = Keypair.generate();
-    const publicKey = keypair.publicKey.toString()
+    const publicKey = keypair.publicKey.toString();
     console.log(`Generated new KeyPair. Wallet PublicKey: `, publicKey);
-    
-    fs.writeFile('addresses', `${publicKey}\n`,  { encoding: 'utf8', flag: 'a' },function(err) {
+
+    let ata = new PublicKey(publicKey);
+
+    try {
+        let destinationAccount = await getOrCreateAssociatedTokenAccount(
+            SOLANA_CONNECTION,
+            signer,
+            new PublicKey(MINT_ADDRESS),
+            keypair.publicKey
+        );
+        ata = destinationAccount.address;
+    } catch (err) {
+        console.log('getOrCreateAssociatedTokenAccount failed:', err)
+        return -1;
+    }
+
+    fs.writeFile('addresses', `${publicKey}\n`, { encoding: 'utf8', flag: 'a' }, function (err) {
+        if (err) throw err;
+    });
+
+    fs.writeFile('atas', `${ata.toString()}\n`, { encoding: 'utf8', flag: 'a' }, function (err) {
         if (err) throw err;
     });
 }
 
-
-for(let i = 0; i < 10000;i++){
-    genAddress();
+async function main() {
+    for (let i = 0; i < 1518; i++) {
+        await genAddress();
+    }
 }
+
+main();
